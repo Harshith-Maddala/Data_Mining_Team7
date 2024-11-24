0c31374445a051c745f7bf5adffbec33edc36274
Neeraj Shashikant Magadum <71748375+neerajmagadum@users.noreply.github.com>
2024-11-24 13:36:26 -0500

    Basic EDA (#1)

---
+++ Game.py
@@ -0,0 +1,75 @@
+#%%[markdown]
+#
+# # Beyond the Play Button : Insights and Trends from Steam’s Game Library
+# ## By: Neeraj Shashikant Magadum, Aditya Kanbargi, Harshith Maddala, Sanjana Muralidhar
+# ### Date: 17 Nov
+#
+# 
+
+#%%[markdown]
+# ## 1. Importing Required Libraries
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import rfit 
+
+
+#%% [markdown]
+# ## 2. Loading the Dataset
+games_df = pd.read_csv('games.csv')
+
+# Display the first 5 rows of the dataset to get an overview of its structure and content
+print(games_df.head())
+
+
+# %% [markdown]
+# ## 3. Basic Exploration of the Dataset
+
+# Check the shape of the dataset (number of rows and columns)
+games_df.shape
+
+# Check the data types of each column to understand what kind of data we are working with
+games_df.dtypes
+
+# Check for missing values in each column; this helps us identify if any data cleaning is needed
+print(games_df.isnull().sum())
+
+# %% [markdown]
+# ## 4. Descriptive Statistics for Numerical Columns
+
+# Get summary statistics for numerical columns in the dataset (e.g., mean, min, max, etc.)
+print(games_df.describe())
+
+
+# %% [markdown]
+# ## 5. Data Cleaning: Dropping Missing Values and Normalizing Genres
+# Define the columns to remove from the dataset
+removed_columns = ['AppID', 'Screenshots', 'Reviews', 'Header image', 'Website', 'Support url', 
+                   'Support email', 'Metacritic url', 'Notes', 'Average playtime two weeks', 
+                   'Median playtime two weeks', 'Median playtime forever', 'Movies', 'Release date',
+                   'Score rank', 'Tags']
+
+# Drop these columns from the dataset
+games_df_cleaned = games_df.drop(columns=removed_columns).copy()
+
+# # Print the "Genres" column to inspect its content before further processing.
+print(games_df_cleaned["Genres"])
+
+# # Convert all genre names to lowercase for consistency (this helps in grouping similar genres later).
+games_df_cleaned["Genres"] = games_df_cleaned["Genres"].str.lower()
+
+games_df_cleaned = games_df_cleaned.dropna(subset=['Categories'])
+games_df_cleaned = games_df_cleaned.dropna(subset=['Genres'])
+
+games_df_cleaned['Developers'].fillna('Unknown', inplace=True)
+games_df_cleaned['Publishers'].fillna('Unknown', inplace=True)
+
+# %% [markdown]
+# ## 6. Final Check for Missing Values After Cleaning
+
+# After cleaning, check if there are any remaining missing values in the cleaned dataset.
+print(games_df_cleaned.isnull().sum())
+games_df_cleaned.shape
+# %%
